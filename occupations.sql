937231dc556a83091e9f7913f01e84c0770d70f1
shrutiparmar2003 <shrutiparmar01082003@gmail.com>
2025-05-23 16:58:14 +0530

    Challenge 5

---
+++ occupations.sql
@@ -0,0 +1,31 @@
+/*Pivot the Occupation column in OCCUPATIONS so that each Name is sorted alphabetically and displayed underneath its corresponding Occupation. The output should consist of four columns (Doctor, Professor, Singer, and Actor) in that specific order, with their respective names listed alphabetically under each column.
+
+Note: Print NULL when there are no more names corresponding to an occupation.
+Occupation will only contain one of the following values: Doctor, Professor, Singer or Actor.
+
+Sample Output
+
+Jenny    Ashley     Meera  Jane
+Samantha Christeen  Priya  Julia
+NULL     Ketty      NULL   Maria
+Explanation
+
+The first column is an alphabetically ordered list of Doctor names.
+The second column is an alphabetically ordered list of Professor names.
+The third column is an alphabetically ordered list of Singer names.
+The fourth column is an alphabetically ordered list of Actor names.
+The empty cell data for columns with less than the maximum number of names per occupation (in this case, the Professor and Actor columns) are filled with NULL values.*/
+
+SELECT
+  MAX(CASE WHEN Occupation = 'Doctor' THEN Name END) AS Doctor,
+  MAX(CASE WHEN Occupation = 'Professor' THEN Name END) AS Professor,
+  MAX(CASE WHEN Occupation = 'Singer' THEN Name END) AS Singer,
+  MAX(CASE WHEN Occupation = 'Actor' THEN Name END) AS Actor
+FROM (
+  SELECT 
+    Name, 
+    Occupation, 
+    ROW_NUMBER() OVER (PARTITION BY Occupation ORDER BY Name) AS rn
+  FROM OCCUPATIONS
+) AS Ranked
+GROUP BY rn;
