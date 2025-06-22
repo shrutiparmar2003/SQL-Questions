d956b1be752b64cf664fb2af44e9980a93aa5601
shrutiparmar2003 <shrutiparmar01082003@gmail.com>
2025-06-22 15:32:23 +0530

    challenge 28

---
+++ weather_observation_12.sql
@@ -0,0 +1,17 @@
+/*Query the list of CITY names from STATION that do not start with vowels and do not end with vowels.
+Your result cannot contain duplicates.*/
+
+select distinct CITY from station
+where
+city  not like 'a%' and
+city  not like 'e%' and
+city  not like 'i%' and
+city  not like 'o%' and
+city  not like 'u%' and
+city  not like '%a' and
+city  not like '%e' and
+city  not like '%i' and
+city  not like '%o' and
+city  not like '%u' ;
+
+
